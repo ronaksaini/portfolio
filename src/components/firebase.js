f3ddc1033e11bfa873b8bd59f3e6297fe8a74f42
RONAK SAINI <92773874+ronaksaini@users.noreply.github.com>
2024-03-30 23:10:12 +0530

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,16 +1,1 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBuPGT7jkeoDY7d03zIJPYL5Nsv022K7AI",
-  authDomain: "portfolio-app-2f621.firebaseapp.com",
-  projectId: "portfolio-app-2f621",
-  storageBucket: "portfolio-app-2f621.appspot.com",
-  messagingSenderId: "164282744685",
-  appId: "1:164282744685:web:47cce7ee9e1d564b14a464",
-  measurementId: "G-25TSGRGY21",
-  databaseURL:"https://portfolio-app-2f621-default-rtdb.firebaseio.com",
-};
-
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
